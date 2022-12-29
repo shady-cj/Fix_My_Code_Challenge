dd789d00c09b751b334b9e0638a47b111ddc1a96
peter <petersp2000@gmail.com>
2022-12-29 20:57:22 +0100

    Fixing task #1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class which calculates the area and
+perimeter of the square from the dimensions passed in
+"""
+
 
 class square():
+    """
+    Initialize the class variables as a default
+    to fall back to
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes and set the args as instance
+        variables
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,9 +26,12 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Calculates the perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Returns a string representation of the square
+        dimension """
         return "{}/{}".format(self.width, self.height)
 
 
